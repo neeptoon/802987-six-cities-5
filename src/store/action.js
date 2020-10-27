282cc3d8207a1de7b4dd4e2a38eb9cac5e318054
neeptoon <neeptoon@mail.ru>
2020-10-27 13:12:07 +0300

    Создает reducer и action

---
+++ src/store/action.js
@@ -0,0 +1,4 @@
+export const ActionType = {
+  CHANGE_CITY: `CHANGE_CITY`,
+  RECIEVE_OFFER: `RECIEVE_OFFER`
+};
---
+++ src/store/reducer.js
@@ -0,0 +1,21 @@
+import ActionType from './action.js';
+import {extend} from '../../utils.js';
+
+let initialState = {
+  cityName: ``,
+  offersList: []
+};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.CHANGE_CITY:
+      return extend(state, {cityName: action.cityName});
+
+    case ActionType.RECIEVE_OFFER:
+      return extend(state, {offersList: action.offersList});
+  }
+
+  return state;
+};
+
+
